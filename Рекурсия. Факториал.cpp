507f349cc7e85dc60943550582cccaaf0fd42877
vania-ilin <56587376+vania-ilin@users.noreply.github.com>
2026-09-25 17:36:43 +0500

    Add files via upload

---
+++ Рекурсия. Факториал.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int Fact ( int N )
+{
+  int F;   
+  printf("input N= %d \n ", N);
+  if ( N <= 1 ) F = 1;
+  else F = N * Fact(N - 1);
+  printf("output N= %d \n ", N);
+  return F;
+}
+void main()
+{
+  int N;
+  printf("Введите натуральное число: ");
+  cin >> N;
+  cout << Fact(N);
+  cin.get(); cin.get();
+}
